e34106a986d7ec0470a9b690ce4afa66c0ac7d92
Dmitrii <cf.gwod@gmail.com>
2023-09-27 18:53:08 +0300

    hashouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import 'normalize.css'
@@ -12,8 +12,8 @@ import App from './pages/App'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter basename="/repo-name">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 )
