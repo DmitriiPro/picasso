6148c3d742471595d1f8807ce2e527273d57c65d
Dmitrii <cf.gwod@gmail.com>
2023-09-27 16:34:27 +0300

    fix route

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -3,14 +3,16 @@ import { Layout } from './Layout'
 import { Post } from './Post'
 import { NoPage } from './NoPage'
 
+import { Navigate } from 'react-router-dom'
+
 export const Router = () => {
   return (
     <Routes>
       <Route path="/">
         <Route index element={<Layout />} />
         <Route path="post/:id" element={<Post />} />
+        <Route path="*" element={<Navigate to="." replace />} />
       </Route>
-      <Route path="*" element={<NoPage />} />
     </Routes>
   )
 }
